70351cc823f66a32440b30b8eb5f38bf03404ded
ericcai0814 <d4070707@gmail.com>
2025-06-26 03:25:54 +0800

    feat(unocss): 新增 presetIcons 以客製化 icon

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,5 @@
 import config from '@slidev/client/uno.config.ts'
-import { mergeConfigs, presetWebFonts } from 'unocss'
+import { mergeConfigs, presetIcons, presetWebFonts } from 'unocss'
 
 export default mergeConfigs([
   config,
@@ -8,6 +8,13 @@ export default mergeConfigs([
       'text-gradient': 'text-transparent bg-clip-text bg-gradient-to-tl from-green-400 via-teal-400 to-blue-500',
     },
     presets: [
+    presetIcons({
+      scale: 1.2,
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
       presetWebFonts({
         fonts: {
           mono: 'DM Mono',
